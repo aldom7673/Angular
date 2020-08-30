f1ad704d6b001ca2016a9b0cea20ddda1e41f30b
aldom7673 <aldom7673@gmail.com>
2020-08-30 00:39:02 -0500

    Single Page Applications P2

--- Week1/conFusion/src/app/app-routing/routes.ts
+++ Week1/conFusion/src/app/app-routing/routes.ts
@@ -9,8 +9,8 @@ import { ContactComponent } from '../contact/contact.component';
 export const routes : Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
-    {path: 'dishdetail', component: DishdetailComponent},
     {path: 'about', component: AboutComponent},
     {path: 'contactus', component: ContactComponent},
-    {path: '', redirectTo : '/home', pathMatch: 'full'}
+    {path: '', redirectTo : '/home', pathMatch: 'full'},
+    {path: 'dishdetail/:id', component: DishdetailComponent }
 ];
